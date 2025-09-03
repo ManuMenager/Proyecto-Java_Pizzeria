c990c3983cbf46bf29dac0ae3925339b02da9308
ManuMenager <emanuelmenager2003@gmail.com>
2025-09-03 13:41:11 -0300

    Agrega implementación de la clase Cliente

--- src/pizzeria/cliente/Cliente.java
+++ src/pizzeria/cliente/Cliente.java
@@ -1,5 +1,39 @@
 package pizzeria.cliente;
 
 public class Cliente {
-
+	private int id;
+	private String nombre;
+	private String direccion;
+	private int telefono;
+	
+	public Cliente(int id, String nombre, String direccion, int telefono) {
+		this.id = id;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public int getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
 }
